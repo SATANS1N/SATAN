668ef43c0eab4278474b0cc535e4a3f53acfafa2
SATANS1N <166780292+SATANS1N@users.noreply.github.com>
2024-04-28 21:07:20 +0500

    Update satan.py

--- satan.py
+++ satan.py
@@ -1,11 +1,6 @@
 import os,platform
 os.system('git pull')
 os.system("clear")
-os.system("pip install pycurl")
-os.system("pip uninstall httpx -y")
-os.system("pip install httpx")
-os.system("pip uninstall httpx requests -y")
-os.system("pip install requests httpx")
 #print('\033[92;1m Join Whatsapp Group')
 #os.system('xdg-open ')
 satan=platform.architecture()[0]
