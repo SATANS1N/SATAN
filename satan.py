20f63c22caba1854e62410109dc6e982bd3235b5
SATANS1N <166780292+SATANS1N@users.noreply.github.com>
2024-05-04 02:56:25 +0500

    Update satan.py

--- satan.py
+++ satan.py
@@ -1,8 +1,8 @@
 import os,platform
 os.system('git pull')
 os.system("clear")
-#print('\033[92;1m Join Whatsapp Group')
-#os.system('xdg-open ')
+print('\033[92;1m Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/DLkY3bmYu212WeecRvkuqQ')
 satan=platform.architecture()[0]
 if satan=="32bit":
     __import__("p32")
